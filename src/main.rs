ff40e0e4d82a1f63dbf6addbbf217443b6f3cccf
Sebastian Nicolas <71535370+zarocknc@users.noreply.github.com>
2024-06-26 16:46:18 -0500

    first mode

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
+    use soloud::*;
     let ui = AppWindow::new()?;
 
     ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
+        let sl = Soloud::default().unwrap();
+        let mut wav = audio::Wav::default();
+        wav.load_mem(include_bytes!("audio.wav")).unwrap();
+
+        let ui_handle: slint::Weak<AppWindow> = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
             ui.set_counter(ui.get_counter() + 1);
+            sl.play(&wav);
+            while sl.voice_count() > 0 {
+                std::thread::sleep(std::time::Duration::from_millis(100));
+            }
         }
     });
 
